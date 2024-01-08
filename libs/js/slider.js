5802d305d6369db615621e539d9da67a3db65d53
Javohir0888 <javohirqurbonov321@gmail.com>
2024-01-08 09:31:03 +0500

    vv

---
+++ libs/js/slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true
+  })
