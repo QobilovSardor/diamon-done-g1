7f3eb034aa5cf9a59f8d87a9625776aa65d59990
Jaxongir Toshpolatov <jaxongirtoshpolatov76@gmail.com>
2024-01-09 20:57:11 +0500

    more changes

--- js/script.js
+++ js/script.js
@@ -10,3 +10,31 @@ var swiper = new Swiper(".imgSlider", {
     clickable: true,
   },
 });
+
+// navbar
+
+const items = document.querySelectorAll(".nav-item");
+const down = document.querySelectorAll(".downBox");
+
+items.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    var icon = item.querySelector(".arrow-icon");
+    var downBox = item.querySelector(".downBox");
+    if (icon) {
+      icon.style.opacity = "0";
+    }
+    downBox.style.display = "inline-block";
+    downBox.style.transition = "all .5s linear";
+  });
+
+  item.addEventListener("mouseout", () => {
+    var icon = item.querySelector(".arrow-icon");
+    var downBox = item.querySelector(".downBox");
+    if (icon) {
+      icon.style.opacity = "1";
+    }
+
+    downBox.style.display = "none";
+    downBox.style.transition = "all .5s linear";
+  });
+});
